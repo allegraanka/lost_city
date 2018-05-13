97a83dadd44a9551318a46759a6bdb33dccfbe69
Allegra Anka <allegra.anka@gmail.com>
2018-05-13 13:09:30 -0400

    users can log in, edit and delete posts, and comment on posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
 
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
+  before_action :owned_post, only: [:edit, :update, :destroy]
 
   def index
     @posts = Post.all.order("created_at DESC")
@@ -17,7 +18,7 @@ class PostsController < ApplicationController
       flash[:success] = "Your post has been created!"
       redirect_to posts_path
     else
-      flash.now[:alert] = "Your new post couldn't be created!  Please check the form."
+      flash.now[:alert] = "Your new post couldn't be created! Please check the form."
       render :new
     end
   end
@@ -33,7 +34,7 @@ class PostsController < ApplicationController
       flash[:success] = "Post updated."
       redirect_to posts_path
     else
-      flash.now[:alert] = "Update failed.  Please check the form."
+      flash.now[:alert] = "Update failed. Please check the form."
       render :edit
     end
   end
@@ -51,6 +52,13 @@ class PostsController < ApplicationController
 
   def set_post
     @post = Post.find(params[:id])
+  end
+
+  def owned_post
+    unless current_user == @post.user
+      flash[:alert] = "That post doesn't belong to you!"
+      redirect_to root_path
+    end
   end
 
 end
