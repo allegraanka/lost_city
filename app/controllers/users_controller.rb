144a5c9524953a902fe3019d2839b87f4e34cb5b
Allegra Anka <allegra.anka@gmail.com>
2018-05-21 17:35:54 -0400

    added bio and edit bio to users functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,29 @@ class  UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @posts = @user.posts
+  end
+
+  def edit_bio
+    @user = current_user
+    @bio = @user.bio
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(bio_params)
+      flash[:success] = "bio updated."
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "update failed. please check the form."
+      render :edit_bio
+    end
+  end
+
+  private
+
+  def bio_params
+    params.require(:user).permit(:bio)
   end
 
 end
