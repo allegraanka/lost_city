2557d201180723382e8d7a52b8dd6751fb6a1f1d
Allegra Anka <allegra.anka@gmail.com>
2018-05-23 13:34:48 -0400

    styling forms throughout app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def sunrise_api
-    uri = URI("https://api.sunrise-sunset.org/json?lat=39.952583&lng=-75.165222&date=today")
+    uri = URI("https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=today")
     @api_data = Net::HTTP.get(uri)
     @api_data
   end
